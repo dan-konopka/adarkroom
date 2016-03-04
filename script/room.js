0c89bfdb70a817ea4efcd5bb917ef11941038a1a
dan-konopka <daniel.konopka@mymail.champlain.edu>
2016-03-03 20:05:23 -0500

    Added css and script folders of updated code

--- script/room.js
+++ script/room.js
@@ -43,7 +43,7 @@ var Room = {
 		'hut': {
 			name: _('hut'),
 			button: null,
-			maximum: 20,
+			maximum: 60,
 			availableMsg: _("builder says there are more wanderers. says they'll work, too."),
 			buildMsg: _('builder puts up a hut, out in the forest. says word will get around.'),
 			maxMsg: _('no more room for huts.'),
@@ -51,7 +51,7 @@ var Room = {
 			cost: function() {
 				var n = $SM.get('game.buildings["hut"]', true);
 				return {
-					'wood': 100 + (n*50)
+					'wood': 10 + (n*5)
 				};
 			}
 		},
